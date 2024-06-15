038518332239e6a2e9daaf639d85c4172d4b104e
Alessandro Rocco Scisca <ale.scisca2@gmail.com>
2024-06-15 22:18:41 +0100

    Working version

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-C_FLAGS = "-Wall"
+C_FLAGS = -std=c89 -include stdio.h -include unistd.h -include time.h
 
 quine-clock: main.c
-	${CC} -o $@ ${C_FLAGS} $<
+	gcc -o $@ ${C_FLAGS} $<
--- main.c
+++ main.c
@@ -1,76 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
-
-time_t t;
-char *src, *p, *e;
-int font[] = {31599, 19812, 14479, 31207, 23524, 29411, 29679, 30866, 31727, 31719, 1040, 0};
-int h, m, s;
-
-int x = 0;
-int y = 0;
-
-#define NUM_DIGITS 8
-#define DIGIT_WIDTH 3
-#define DIGIT_PADDING 2
-#define DIGIT_HEIGHT 5
-void pixel(char c) {
-    int digit_pos = x / (DIGIT_WIDTH + DIGIT_PADDING);
-    int d;
-    switch (digit_pos) {
-        case 0: d = h / 10; break;
-        case 1: d = h % 10; break;
-        case 3: d = m / 10; break;
-        case 4: d = m % 10; break;
-        case 6: d = s / 10; break;
-        case 7: d = s % 10; break;
-        case 2: case 5: d = 10; break;
-        default: d = 11;
-    }
-    int dx = x % (DIGIT_WIDTH + DIGIT_PADDING);
-    int on = dx < DIGIT_WIDTH && font[d] >> (dx + DIGIT_WIDTH * (DIGIT_HEIGHT - y)) & 1;
-    if (dx <= NUM_DIGITS * DIGIT_WIDTH && y <= DIGIT_HEIGHT && on) {
-        printf("\e[41m%c\e[0m", c);
-    } else {
-        putchar(c);
-    }
-    if (c != '\n') {
-        x++;
-    } else {
-        x = 0;
-        y++;
-    }
-}
-
-int main() {
-    src = "..............................................\n..............................................\n..............................................\n..............................................\n..............................................\n..............................................\n..............................................\n..............................................\n";
-    while (1) {
-        p = src;
-        e = src;
-        time(&t);
-        s = t % (3600 * 24);
-        h = s / 3600;
-        m = (s % 3600) / 60;
-        s = s % 60;
-        x = 0;
-        y = 0;
-        while (*p) {
-            if (*p != 0x40) pixel(*p);
-            else while (*e) {
-                char c = *e;
-                switch(c) {
-                    case '\n':
-                        c = 'n';
-                    case '\\': case '"':
-                        pixel('\\');
-                }
-                pixel(c);
-                e++;
-            }
-            p++;
-        }
-        sleep(1);
-        printf("\033[%dA\033[%dD", y, x);
-    }
-    return 0;
-}
+time_t t;char*p,*e;extern char*i;x,y,h,m,s,c,d[8],f[]={31599,19812,14479,31207,
+23524,29411,29679,30866,31727,31719,1040};o(c){d[0]=h/10;d[3]=m/10;d[7]=s%10;d[
+4]=m%10;d[1]=h%10;d[6]=s/10;d[2]=d[5]=10;printf((x<80&&y<=10&&x%10<6&&f[d[x/10]
+]>>((x/2)%5+15-3*(y/2))&1)?"\e[41m%c\e[0m":"%c",c);x++;if(c==10){x=0;y++;}}main
+(){for(;;){p=e=i;time(&t);s=t%86400;h=s/3600;m=(s%3600)/60;s%=60;x=0;y=0;while(
+*p){if(*p!=64)o(*p);else while(*e){c=*e;switch(c){case 10:c='n';case 92:case 34
+:o(92);}o(c);if(x>77){o(92);o(10);}e++;}p++;}sleep(1);printf("\033[%dA\033[%dD"
+,y,x);}}char*i="time_t t;char*p,*e;extern char*i;x,y,h,m,s,c,d[8],f[]={31599,1\
+9812,14479,31207,\n23524,29411,29679,30866,31727,31719,1040};o(c){d[0]=h/10;d[\
+3]=m/10;d[7]=s%10;d[\n4]=m%10;d[1]=h%10;d[6]=s/10;d[2]=d[5]=10;printf((x<80&&y\
+<=10&&x%10<6&&f[d[x/10]\n]>>((x/2)%5+15-3*(y/2))&1)?\"\\e[41m%c\\e[0m\":\"%c\"\
+,c);x++;if(c==10){x=0;y++;}}main\n(){for(;;){p=e=i;time(&t);s=t%86400;h=s/3600\
+;m=(s%3600)/60;s%=60;x=0;y=0;while(\n*p){if(*p!=64)o(*p);else while(*e){c=*e;s\
+witch(c){case 10:c='n';case 92:case 34\n:o(92);}o(c);if(x>77){o(92);o(10);}e++\
+;}p++;}sleep(1);printf(\"\\033[%dA\\033[%dD\"\n,y,x);}}char*i=\"@\";\n";
